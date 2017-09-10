d8781ea3db441a9835cda66cfd56591327127979
Raj-Varun <varuncr7raj@gmail.com>
2017-09-10 21:48:02 +0530

    Chat bot updated

--- app/src/main/java/com/rachelgrau/rachel/health4theworldstroke/Activities/ChatBotMainActivity.java
+++ app/src/main/java/com/rachelgrau/rachel/health4theworldstroke/Activities/ChatBotMainActivity.java
@@ -1,13 +1,13 @@
 package com.rachelgrau.rachel.health4theworldstroke.Activities;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import ai.api.AIListener;
 import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
@@ -17,27 +17,36 @@ import ai.api.model.AIError;
 import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
-import com.google.gson.JsonElement;
-import java.util.Map;
+
+import java.util.ArrayList;
+
 
 import com.rachelgrau.rachel.health4theworldstroke.R;
-import com.squareup.picasso.Downloader;
 
 public class ChatBotMainActivity extends AppCompatActivity implements AIListener  {
 
-    private AIService aiService;
-    TextView tv;
-    Button listenButton;
+    AIService aiService;
     AIDataService aiDataService;
     AIConfiguration config;
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+
+    private boolean rightSide = true; //true if you want message on right rightSide
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot_main);
 
-        listenButton = (Button) findViewById(R.id.button);
-        tv = (TextView) findViewById(R.id.textView2);
+        listView = (ListView) findViewById(R.id.msgview);
+        chatText = (EditText) findViewById(R.id.userMesg);
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
+        listView.setAdapter(chatArrayAdapter);
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
 
         config = new AIConfiguration("15d1973433dd45df966ef1b4fc750daf",
                 AIConfiguration.SupportedLanguages.English,
@@ -45,23 +54,20 @@ public class ChatBotMainActivity extends AppCompatActivity implements AIListener
 
         // Use with text search
         aiDataService = new AIDataService(this, config);
-
         // Use with Voice input
         aiService = AIService.getService(this, config);
-
         aiService.setListener(this);
-
         }
-        public void listenButtonOnClick(View view) {
-            AIRequest aiRequest = new AIRequest();
-                aiRequest.setQuery("How are you");
+        public void send(View v) {
+                String the_msg=chatText.getText().toString();
+                sendChatMessage(the_msg);
+                AIRequest aiRequest = new AIRequest();
+                aiRequest.setQuery(the_msg);
 
-                if(aiRequest==null) {
+                if(aiRequest.equals("")) {
                     throw new IllegalArgumentException("aiRequest must be not null");
                 }
-
-                final AsyncTask<AIRequest, Integer, AIResponse> task =
-                        new AsyncTask<AIRequest, Integer, AIResponse>() {
+                final AsyncTask<AIRequest, Integer, AIResponse> task = new AsyncTask<AIRequest, Integer, AIResponse>() {
                             private AIError aiError;
                             @Override
                             protected AIResponse doInBackground(final AIRequest... params) {
@@ -89,42 +95,34 @@ public class ChatBotMainActivity extends AppCompatActivity implements AIListener
             @Override
             public void onResult(final AIResponse response) {
                 Result result = response.getResult();
-
-                // Get parameters
-                String parameterString = "";
-                if (result.getParameters() != null && !result.getParameters().isEmpty()) {
-                    for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
-                        parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
-                    }
-                }
-                //Show results in TextView.
-                tv.setText("Query:" + result.getResolvedQuery() +
-                        "\nAction: " + result.getFulfillment().getSpeech() +
-                        "\nParameters: " + parameterString);
+                sendResponse(result.getFulfillment().getSpeech());
             }
-
+            private boolean sendResponse(String text) {
+                if (text.length() == 0)
+                    return false;
+                chatArrayAdapter.add(new ChatMessage(!rightSide, text));
+                return true;
+            }
+            private boolean sendChatMessage(String text) {
+                if (text.length() == 0)
+                    return false;
+                chatArrayAdapter.add(new ChatMessage(rightSide, text));
+                chatText.setText("");
+                return true;
+            }
             @Override
             public void onError(AIError error) {
-
             }
-
             @Override
             public void onAudioLevel(float level) {
-
             }
-
             @Override
             public void onListeningStarted() {
-
             }
-
             @Override
             public void onListeningCanceled() {
-
             }
-
             @Override
             public void onListeningFinished() {
-
             }
         }
