f2f2c5306d85340308fff90e4657488d0eea9fcd
mpfarmer <mrcodingfarmer@gmail.com>
2017-10-14 16:25:00 -0700

    Fix the UI Problems in MainActivity2 Updated UI to match iOS

--- app/src/main/java/com/rachelgrau/rachel/health4theworldstroke/Activities/MainActivity2.java
+++ app/src/main/java/com/rachelgrau/rachel/health4theworldstroke/Activities/MainActivity2.java
@@ -2,8 +2,8 @@ package com.rachelgrau.rachel.health4theworldstroke.Activities;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
@@ -19,7 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main2_new);
         setUpToolbar();
     }
     private void setUpToolbar() {
@@ -31,31 +31,58 @@ public class MainActivity2 extends AppCompatActivity {
         toolbarTitle.setTypeface(font);
         setSupportActionBar(myToolbar);
     }
-    public void onClick(View v){
 
-        if(v.getId()==R.id.helpMeSpeakButton || v.getId()==R.id.imageView1 || v.getId()==R.id.text1 ){
-            Intent intent=new Intent(this,HelpMeSpeakActivity.class);
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.ll_help_me_speak) {
+            Intent intent = new Intent(this, HelpMeSpeakActivity.class);
             startActivity(intent);
-        }if(v.getId()==R.id.exercise_button || v.getId()==R.id.imageView2 || v.getId()==R.id.text2 ){
+        }
+        if (v.getId() == R.id.ll_exercise) {
             Intent intent = new Intent(this, exercise.class);
-            intent.putExtra(data,"o");
+            intent.putExtra(data, "o");
             startActivity(intent);
-        }if(v.getId()==R.id.learnButton || v.getId()==R.id.imageView3 || v.getId()==R.id.text3 ){
+        }
+        if (v.getId() == R.id.ll_learn) {
             Intent intent = new Intent(this, LearnActivity.class);
             intent.putExtra(EXTRA_PAGE_TYPE, PAGE_TYPE_READ);
             startActivity(intent);
-        }if(v.getId()==R.id.remindersButton || v.getId()==R.id.imageView4 || v.getId()==R.id.text4 ){
+        }
+        if (v.getId() == R.id.ll_reminders) {
             Intent intent = new Intent(this, RemindersActivity.class);
             startActivity(intent);
-        }if(v.getId()==R.id.generalInfoButton || v.getId()==R.id.imageView5 || v.getId()==R.id.text5 ){
+        }
+        if (v.getId() == R.id.ll_general_info) {
             /*Intent intent = new Intent(this, InfoListActivity.class);*/
             Intent intent = new Intent(this, ChatBotMainActivity.class);
             startActivity(intent);
-        }if(v.getId()==R.id.surveyButton || v.getId()==R.id.imageView6 || v.getId()==R.id.text6 ){
-            Intent intent=new Intent(this,password_gateway.class);
+        }
+        if (v.getId() == R.id.ll_survey) {
+            Intent intent = new Intent(this, password_gateway.class);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.iv_feeling_1) {
+            Intent intent = new Intent(this, ChatBotMainActivity.class);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.iv_feeling_2) {
+            Intent intent = new Intent(this, ChatBotMainActivity.class);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.iv_feeling_3) {
+            Intent intent = new Intent(this, ChatBotMainActivity.class);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.iv_feeling_4) {
+            Intent intent = new Intent(this, ChatBotMainActivity.class);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.iv_feeling_5) {
+            Intent intent = new Intent(this, ChatBotMainActivity.class);
             startActivity(intent);
         }
     }
+
     public void onBackPressed() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
